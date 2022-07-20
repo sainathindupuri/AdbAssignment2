8332655ea46d82494ef98e0dc8efa3a875c6fd52
sainath <sainathindupuri@gmail.com>
2022-07-20 00:47:17 -0500

    added 2 and 3 chnages

--- app.py
+++ app.py
@@ -16,6 +16,48 @@ cursor = connection.cursor()
 def Hello():
     return render_template('index.html')
 
+@app.route('/ShowNLargest', methods=['GET', 'POST'])
+def showDetails():
+    cursor = connection.cursor()    
+    num1 = int(request.form.get("num1"))    
+    cursor.execute("select top {} * from dbo.all_month ORDER BY MAG DESC".format(num1))
+    data = cursor.fetchall()
+    return render_template('ShowNLargest.html', data = data)  
+
+@app.route('/Show500KmEarthquakes', methods=['GET', 'POST'])
+def show500KmEarthquakes():
+    cursor = connection.cursor()   
+    degree = (500/111)
+    latMax = 32.7355816 + degree
+    latMin = 32.7355816- degree
+    longMax = -97.1071186 + degree
+    longMin = -97.1071186 - degree   
+    param_data = (latMax,latMin,longMax,longMin)
+    print(" limits are ",param_data)
+    cursor.execute("select * from dbo.all_month where latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ?" , param_data)
+
+    data = cursor.fetchall()
+    print(len(data))
+    return render_template('Show500KmEarthquakes.html', data = data)  
+
+
+@app.route('/QuakesGreaterThan3', methods=['GET', 'POST'])
+def quakesGreaterThan3():
+    cursor = connection.cursor()   
+    startDate = request.form.get("dateStart")
+    endDate = request.form.get("dateEnd")
+    print("Start date : ",startDate,"Type is ",type(startDate))
+    startDate = startDate.replace("-","/")
+    endDate = endDate.replace("-","/")
+    print("Start date : ",startDate,"Type is ",type(startDate))
+    query_string = "select * from all_month where time >= '"+startDate+"' and time <= '"+endDate+"'  AND mag > 3"
+    cursor.execute(query_string)
+    data = cursor.fetchall()
+    print(len(data))
+    return render_template('QuakesGreaterThan3.html', data = data)  
+
+
+ 
 
 
 if __name__ == '__main__':    
